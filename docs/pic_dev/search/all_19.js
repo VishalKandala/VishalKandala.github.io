753cd162fd02f2239fa1f9f7c78d6f75b44f3baa
gh-actions <actions@github.com>
2026-02-11 14:41:08 +0000

    Update PIC Dev Doxygen docs

--- docs/pic_dev/search/all_19.js
+++ docs/pic_dev/search/all_19.js
@@ -144,7 +144,7 @@ var searchData=
   ['pseudo_5fcfl_5fgrowth_5ffactor_141',['pseudo_cfl_growth_factor',['../variables_8h.html#ab93e16d3feaf58ac559aef07c06e3ed5',1,'SimCtx']]],
   ['pseudo_5fcfl_5freduction_5ffactor_142',['pseudo_cfl_reduction_factor',['../variables_8h.html#a3196abeed1e369450c471d9d7c8b9b53',1,'SimCtx']]],
   ['pseudo_5fperiodic_143',['pseudo_periodic',['../variables_8h.html#a0e447e75b801c0a9305fcadd317159a4',1,'SimCtx']]],
-  ['psi_144',['psi',['../variables_8h.html#af8df0703ce4dab73db61fa500a7c5f2b',1,'UserCtx::Psi'],['../variables_8h.html#aca5183ff602e43d5a3f1d801d7603d63',1,'Particle::psi']]],
+  ['psi_144',['psi',['../variables_8h.html#aca5183ff602e43d5a3f1d801d7603d63',1,'Particle::psi'],['../variables_8h.html#af8df0703ce4dab73db61fa500a7c5f2b',1,'UserCtx::Psi']]],
   ['psi_5fnodal_145',['Psi_nodal',['../variables_8h.html#ac0025b1b394319e0af260c6d472bd9d8',1,'UserCtx']]],
   ['pucont_146',['pUcont',['../variables_8h.html#a9f0e5575e60f124d715fc4e5b2f4d6a4',1,'UserCtx']]],
   ['python_147',['2.1. Installing Build Tools, MPI, and Python',['../01_Installation.html#install_tools_sec',1,'']]]
--- docs/pic_dev/search/variables_10.js
+++ docs/pic_dev/search/variables_10.js
@@ -41,7 +41,7 @@ var searchData=
   ['pseudo_5fcfl_5fgrowth_5ffactor_38',['pseudo_cfl_growth_factor',['../variables_8h.html#ab93e16d3feaf58ac559aef07c06e3ed5',1,'SimCtx']]],
   ['pseudo_5fcfl_5freduction_5ffactor_39',['pseudo_cfl_reduction_factor',['../variables_8h.html#a3196abeed1e369450c471d9d7c8b9b53',1,'SimCtx']]],
   ['pseudo_5fperiodic_40',['pseudo_periodic',['../variables_8h.html#a0e447e75b801c0a9305fcadd317159a4',1,'SimCtx']]],
-  ['psi_41',['psi',['../variables_8h.html#aca5183ff602e43d5a3f1d801d7603d63',1,'Particle::psi'],['../variables_8h.html#af8df0703ce4dab73db61fa500a7c5f2b',1,'UserCtx::Psi']]],
+  ['psi_41',['psi',['../variables_8h.html#af8df0703ce4dab73db61fa500a7c5f2b',1,'UserCtx::Psi'],['../variables_8h.html#aca5183ff602e43d5a3f1d801d7603d63',1,'Particle::psi']]],
   ['psi_5fnodal_42',['Psi_nodal',['../variables_8h.html#ac0025b1b394319e0af260c6d472bd9d8',1,'UserCtx']]],
   ['pucont_43',['pUcont',['../variables_8h.html#a9f0e5575e60f124d715fc4e5b2f4d6a4',1,'UserCtx']]]
 ];
