501fa302ba0ba26e9210d82bf53831729968abc3
gh-actions <actions@github.com>
2026-02-25 07:21:43 +0000

    Update PIC Dev Doxygen docs

--- docs/pic_dev/search/files_b.js
+++ docs/pic_dev/search/files_b.js
@@ -1,16 +1,18 @@
 var searchData=
 [
-  ['particlemotion_2ec_0',['ParticleMotion.c',['../ParticleMotion_8c.html',1,'']]],
-  ['particlemotion_2eh_1',['ParticleMotion.h',['../ParticleMotion_8h.html',1,'']]],
-  ['particlephysics_2ec_2',['ParticlePhysics.c',['../ParticlePhysics_8c.html',1,'']]],
-  ['particlephysics_2eh_3',['ParticlePhysics.h',['../ParticlePhysics_8h.html',1,'']]],
-  ['particleswarm_2ec_4',['ParticleSwarm.c',['../ParticleSwarm_8c.html',1,'']]],
-  ['particleswarm_2eh_5',['ParticleSwarm.h',['../ParticleSwarm_8h.html',1,'']]],
-  ['picsolver_2ec_6',['picsolver.c',['../picsolver_8c.html',1,'']]],
-  ['poisson_2ec_7',['poisson.c',['../poisson_8c.html',1,'']]],
-  ['poisson_2eh_8',['poisson.h',['../poisson_8h.html',1,'']]],
-  ['postprocessing_5fkernels_2ec_9',['postprocessing_kernels.c',['../postprocessing__kernels_8c.html',1,'']]],
-  ['postprocessing_5fkernels_2eh_10',['postprocessing_kernels.h',['../postprocessing__kernels_8h.html',1,'']]],
-  ['postprocessor_2ec_11',['postprocessor.c',['../postprocessor_8c.html',1,'']]],
-  ['postprocessor_2eh_12',['postprocessor.h',['../postprocessor_8h.html',1,'']]]
+  ['particle_5fstatistics_2ec_0',['particle_statistics.c',['../particle__statistics_8c.html',1,'']]],
+  ['particle_5fstatistics_2eh_1',['particle_statistics.h',['../particle__statistics_8h.html',1,'']]],
+  ['particlemotion_2ec_2',['ParticleMotion.c',['../ParticleMotion_8c.html',1,'']]],
+  ['particlemotion_2eh_3',['ParticleMotion.h',['../ParticleMotion_8h.html',1,'']]],
+  ['particlephysics_2ec_4',['ParticlePhysics.c',['../ParticlePhysics_8c.html',1,'']]],
+  ['particlephysics_2eh_5',['ParticlePhysics.h',['../ParticlePhysics_8h.html',1,'']]],
+  ['particleswarm_2ec_6',['ParticleSwarm.c',['../ParticleSwarm_8c.html',1,'']]],
+  ['particleswarm_2eh_7',['ParticleSwarm.h',['../ParticleSwarm_8h.html',1,'']]],
+  ['picsolver_2ec_8',['picsolver.c',['../picsolver_8c.html',1,'']]],
+  ['poisson_2ec_9',['poisson.c',['../poisson_8c.html',1,'']]],
+  ['poisson_2eh_10',['poisson.h',['../poisson_8h.html',1,'']]],
+  ['postprocessing_5fkernels_2ec_11',['postprocessing_kernels.c',['../postprocessing__kernels_8c.html',1,'']]],
+  ['postprocessing_5fkernels_2eh_12',['postprocessing_kernels.h',['../postprocessing__kernels_8h.html',1,'']]],
+  ['postprocessor_2ec_13',['postprocessor.c',['../postprocessor_8c.html',1,'']]],
+  ['postprocessor_2eh_14',['postprocessor.h',['../postprocessor_8h.html',1,'']]]
 ];
