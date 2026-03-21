d9566bba27b95d33c8565cc6079bf40b0704122a
gh-actions <actions@github.com>
2026-03-21 02:40:28 +0000

    Update PICurv Doxygen docs

--- docs/picurv/search/all_0.js
+++ docs/picurv/search/all_0.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['01_5finstallation_2emd_0',['01_Installation.md',['../01__Installation_8md.html',1,'']]],
   ['02_5ftutorial_5fprogrammatic_5fgrid_2emd_1',['02_Tutorial_Programmatic_Grid.md',['../02__Tutorial__Programmatic__Grid_8md.html',1,'']]],
-  ['03_5ftutorial_5ffile_2dbased_5fgrid_2emd_2',['03_Tutorial_File-Based_Grid.md',['../03__Tutorial__File-Based__Grid_8md.html',1,'']]],
-  ['04_5fvisualization_5ftutorial_2emd_3',['04_Visualization_Tutorial.md',['../04__Visualization__Tutorial_8md.html',1,'']]],
-  ['05_5fthe_5fconductor_5fscript_2emd_4',['05_The_Conductor_Script.md',['../05__The__Conductor__Script_8md.html',1,'']]],
-  ['06_5fsimulation_5fanatomy_2emd_5',['06_Simulation_Anatomy.md',['../06__Simulation__Anatomy_8md.html',1,'']]],
-  ['07_5fcase_5freference_2emd_6',['07_Case_Reference.md',['../07__Case__Reference_8md.html',1,'']]],
-  ['08_5fsolver_5freference_2emd_7',['08_Solver_Reference.md',['../08__Solver__Reference_8md.html',1,'']]],
-  ['09_5fmonitor_5freference_2emd_8',['09_Monitor_Reference.md',['../09__Monitor__Reference_8md.html',1,'']]]
+  ['03_2020_2',['8.1 Coverage Follow-Up Snapshot (2026-03-20)',['../40_Testing_and_Quality_Guide.html#p40_coverage_followup_ssec',1,'']]],
+  ['03_5ftutorial_5ffile_2dbased_5fgrid_2emd_3',['03_Tutorial_File-Based_Grid.md',['../03__Tutorial__File-Based__Grid_8md.html',1,'']]],
+  ['04_5fvisualization_5ftutorial_2emd_4',['04_Visualization_Tutorial.md',['../04__Visualization__Tutorial_8md.html',1,'']]],
+  ['05_5fthe_5fconductor_5fscript_2emd_5',['05_The_Conductor_Script.md',['../05__The__Conductor__Script_8md.html',1,'']]],
+  ['06_5fsimulation_5fanatomy_2emd_6',['06_Simulation_Anatomy.md',['../06__Simulation__Anatomy_8md.html',1,'']]],
+  ['07_5fcase_5freference_2emd_7',['07_Case_Reference.md',['../07__Case__Reference_8md.html',1,'']]],
+  ['08_5fsolver_5freference_2emd_8',['08_Solver_Reference.md',['../08__Solver__Reference_8md.html',1,'']]],
+  ['09_5fmonitor_5freference_2emd_9',['09_Monitor_Reference.md',['../09__Monitor__Reference_8md.html',1,'']]]
 ];
