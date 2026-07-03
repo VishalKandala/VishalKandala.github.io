1827b2d4295b81ab92d74c0d02c9987e1ee56e50
gh-actions <actions@github.com>
2026-07-03 01:52:01 +0000

    Update PICurv Doxygen docs

--- docs/picurv/search/variables_8.js
+++ docs/picurv/search/variables_8.js
@@ -2,8 +2,10 @@ var searchData=
 [
   ['handler_0',['handler',['../variables_8h.html#afd962f86b7e71fb2cb7ba60eb76058e8',1,'BoundaryFaceConfig']]],
   ['handler_5ftype_1',['handler_type',['../variables_8h.html#a499ad2619c29a9aee2d01e859c8942f6',1,'BoundaryFaceConfig']]],
-  ['head_2',['head',['../variables_8h.html#af0b49fb7e16779d8954af1c5528d585f',1,'list::head'],['../variables_8h.html#aeebefbaee8441ef28ed3121ceebb27fc',1,'IBMList::head']]],
-  ['head_5fang_3',['head_ang',['../variables_8h.html#a3c873d7c4829ae18c321a737c0577d95',1,'Cstart']]],
-  ['header_5fsuffixes_4',['HEADER_SUFFIXES',['../namespacegenerate__doxygen__fallback__indexes.html#a4f6c199c3c48cde5d36bb0993fd1a33d',1,'generate_doxygen_fallback_indexes']]],
-  ['hydro_5',['hydro',['../variables_8h.html#acdd1a65b6f7557e023f70e782c24c202',1,'SimCtx']]]
+  ['have_5finitial_5fnorm_2',['have_initial_norm',['../momentum__newton__krylov_8c.html#ac0b091bd489616deadfcf0b9a21bc9ef',1,'MomentumNewtonKrylovContext']]],
+  ['head_3',['head',['../variables_8h.html#af0b49fb7e16779d8954af1c5528d585f',1,'list::head'],['../variables_8h.html#aeebefbaee8441ef28ed3121ceebb27fc',1,'IBMList::head']]],
+  ['head_5fang_4',['head_ang',['../variables_8h.html#a3c873d7c4829ae18c321a737c0577d95',1,'Cstart']]],
+  ['header_5fsuffixes_5',['HEADER_SUFFIXES',['../namespacegenerate__doxygen__fallback__indexes.html#a4f6c199c3c48cde5d36bb0993fd1a33d',1,'generate_doxygen_fallback_indexes']]],
+  ['history_5ffile_6',['history_file',['../momentum__newton__krylov_8c.html#a9874825fe312f850f31d798f30c1e9f3',1,'MomentumNewtonKrylovContext']]],
+  ['hydro_7',['hydro',['../variables_8h.html#acdd1a65b6f7557e023f70e782c24c202',1,'SimCtx']]]
 ];
